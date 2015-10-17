cb82c7e58344b1c6722d1de60c304d470489cd98
Natalia Anna Tyszkiewicz <natalka@Natalias-MacBook-Air.local>
2015-10-17 14:17:35 +0200

    add aasm to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,24 @@
 class Order < ActiveRecord::Base
+  include AASM
+  field :state
+
+  aasm do
+    state :new, :initial => true
+    state :in_progress
+    state :collected
+    state :delivered
+
+    event :pick_up_order do
+      transitions :from => :new, :to => :in_progress
+    end
+
+    event :collect_order do
+      transitions :from => :in_progress, :to => :collected
+    end
+
+    event :deliver_to_customer do
+      transitions :co => :collected, :to => :delivered
+    end
+
+  end
 end
