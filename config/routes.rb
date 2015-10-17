0e8d350c9aefbcf3bcee01af96d5f42cf9081ffb
Natalia Anna Tyszkiewicz <natalka@Natalias-MacBook-Air.local>
2015-10-17 15:42:55 +0200

    rm comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
-  # authenticated :user do
-    # root to: 'orders#index', as: :authenticated_root
-    resources :orders, only: [:new, :create, :index]
-  # end
+  resources :orders, only: [:new, :create, :index]
 
 end
